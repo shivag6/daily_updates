7a76733c767fa79a836eca75c0e89ceb86f070f4
shivag6 <shivakumar@geval6.com>
2026-02-26 15:41:59 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# daily_updates
-projects and learning
-<br>
-author - shivakumar
-today i am go through the wordpress and adminer and creat a wordpress and xampp and connect a adminer to the wordpress and extract the data from the adminer
+# Animated-login-page
--- script.js
+++ script.js
@@ -1,55 +1,9 @@
-const input = document.getElementById("taskInput");
-const addBtn = document.getElementById("addBtn");
-const list = document.getElementById("taskList");
-
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-function saveTasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function renderTasks() {
-    list.innerHTML = "";
-
-    tasks.forEach((task, index) => {
-        const li = document.createElement("li");
-
-        li.textContent = task.text;
-        if (task.done) li.classList.add("completed");
-
-        li.onclick = () => {
-            tasks[index].done = !tasks[index].done;
-            saveTasks();
-            renderTasks();
-        };
-
-        const delBtn = document.createElement("button");
-        delBtn.textContent = "X";
-        delBtn.className = "delete-btn";
-
-        delBtn.onclick = (e) => {
-            e.stopPropagation();
-            tasks.splice(index, 1);
-            saveTasks();
-            renderTasks();
-        };
-
-        li.appendChild(delBtn);
-        list.appendChild(li);
-    });
-}
-
-addBtn.onclick = () => {
-    if (input.value.trim() === "") return;
-
-    tasks.push({
-        text: input.value,
-        done: false
-    });
-
-    input.value = "";
-    saveTasks();
-    renderTasks();
-};
-
-renderTasks();
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
